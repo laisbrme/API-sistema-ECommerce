7da1a9dc1545cd93c2b22f62ae83f771ec91484f
Laís Brum Menezes <laisbrme@gmail.com>
2024-09-25 16:11:52 -0300

    :hammer: refactor(service): Atualização do EnderecoService.java para integrar novo tratamento de exceções

--- ecommerce/src/main/java/br/edu/unifaa/ecommerce/service/EnderecoService.java
+++ ecommerce/src/main/java/br/edu/unifaa/ecommerce/service/EnderecoService.java
@@ -1,11 +1,13 @@
 package br.edu.unifaa.ecommerce.service;
 
 import br.edu.unifaa.ecommerce.model.Endereco;
+import br.edu.unifaa.ecommerce.model.exception.ResourceBadRequestException;
+import br.edu.unifaa.ecommerce.model.exception.ResourceNotFoundException;
 import br.edu.unifaa.ecommerce.repository.EnderecoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +19,9 @@ public class EnderecoService {
 
     public Endereco adicionar(Endereco endereco){
 
+        if(endereco.getCep().equals("")){
+            throw new ResourceBadRequestException("O CEP não pode ser vazio.");
+        }
         endereco.setId(0);
 
         return enderecoRepository.save(endereco);
@@ -32,7 +37,7 @@ public class EnderecoService {
         Optional<Endereco> enderecoLocalizado =  enderecoRepository.findById(id);
 
         if(enderecoLocalizado.isEmpty()){
-            throw new InputMismatchException("Não foi possível encontrar um endereço com o id: " + id);
+            throw new ResourceNotFoundException("Não foi possível encontrar um endereço com o id: " + id);
         }
 
         return enderecoLocalizado;
@@ -42,6 +47,9 @@ public class EnderecoService {
 
         obterPorId(id);
         endereco.setId(id);
+        if(endereco.getCep().equals("")){
+            throw new ResourceBadRequestException("O CEP não pode ser vazio.");
+        }
 
         return enderecoRepository.save(endereco);
     }
